42cbb504c817b972f8d05bd4d24514a9bd8e9736
David Mendoza <D.mendoza99@unitec.edu>
2019-04-04 18:36:10 -0600

    estructura basica

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Views/Home";
+import Error404 from "./Views/Error404";
+import Header from "./Components/Header";
+import "./Styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div className="bodybody">
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route component={Error404} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
