7e6312caace98c861cf2b0119a4befca3218c3a4
David Mendoza <D.mendoza99@unitec.edu>
2019-04-04 19:25:52 -0600

    jejeje falle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,51 +1,12 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, NavLink, Collapse } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { FaChartBar, FaBook, FaUser, FaCartArrowDown } from "react-icons/fa";
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-  togglemenu = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
   render() {
     return (
-      <Navbar className="heading" color="primary" expand="md" fixed="top" dark>
-        <NavbarBrand href="/" style={{ color: "white", fontWeight: "bolder" }}>
-          <NavLink to="/">
-            <img alt="Logo" style={{ height: "fill", width: 100 }} />
-          </NavLink>
-        </NavbarBrand>
-        <NavbarToggler className="togglerB" onClick={this.togglemenu} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink
-                onClick={() => {
-                  this.togglemodalsignup();
-                }}
-                className="nav-link"
-                style={{ color: "white" }}
-                to="#">
-                Registrate
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                onClick={() => {
-                  this.togglemodallogin();
-                }}
-                className="nav-link"
-                style={{ color: "white" }}
-                to="#">
-                Iniciar Sesión
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <div class="sidenav">
+        <p>header :v</p>
+      </div>
     );
   }
 }
