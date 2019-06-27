6245f0b49250959ea224b9ce91e704115e320ea6
David Mendoza <dmendoza99@unitec.ede>
2019-06-26 21:05:05 -0600

    inventario

--- Client/src/views/Inventario.js
+++ Client/src/views/Inventario.js
@@ -13,7 +13,7 @@ class Inventario extends Component {
           <Row>
             <Col md={12}>
               <Card
-                title="Edit Profile"
+                title="Agregar"
                 content={
                   <form>
                     <FormInputs
@@ -78,6 +78,120 @@ class Inventario extends Component {
                       properties={[
                         {
                           label: "City",
+                          onChange: () => {
+                            console.log("PENE");
+                          },
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "City",
+                          defaultValue: "Mike",
+                        },
+                        {
+                          label: "Country",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Country",
+                          defaultValue: "Andrew",
+                        },
+                        {
+                          label: "Postal Code",
+                          type: "number",
+                          bsClass: "form-control",
+                          placeholder: "ZIP Code",
+                        },
+                      ]}
+                    />
+
+                    <Row>
+                      <Col md={12}>
+                        <FormGroup controlId="formControlsTextarea">
+                          <ControlLabel>About Me</ControlLabel>
+                          <FormControl
+                            rows="5"
+                            componentClass="textarea"
+                            bsClass="form-control"
+                            placeholder="Here can be your description"
+                            defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Button bsStyle="info" pullRight fill type="submit">
+                      Update Profile
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+              <Card
+                title="Listar"
+                content={
+                  <form>
+                    <FormInputs
+                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "Company (disabled)",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Company",
+                          defaultValue: "Creative Code Inc.",
+                          disabled: true,
+                        },
+                        {
+                          label: "Username",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Username",
+                          defaultValue: "michael23",
+                        },
+                        {
+                          label: "Email address",
+                          type: "email",
+                          bsClass: "form-control",
+                          placeholder: "Email",
+                        },
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-6", "col-md-6"]}
+                      properties={[
+                        {
+                          label: "First name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "First name",
+                          defaultValue: "Mike",
+                        },
+                        {
+                          label: "Last name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Last name",
+                          defaultValue: "Andrew",
+                        },
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-12"]}
+                      properties={[
+                        {
+                          label: "Adress",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Home Adress",
+                          defaultValue: "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09",
+                        },
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-4", "col-md-4", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "City",
+                          onChange: () => {
+                            console.log("PENE");
+                          },
                           type: "text",
                           bsClass: "form-control",
                           placeholder: "City",
