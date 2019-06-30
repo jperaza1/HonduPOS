fb7a9bcccf25c359195d78c4a5f577716833b61c
David Mendoza <dmendoza99@unitec.ede>
2019-06-30 15:35:41 -0600

    no aguanto la cabeza ocupo mucho de ui y no puedo pensar con mi novia haciendo mucho ruido en el telefono

--- Client/src/views/Pos.js
+++ Client/src/views/Pos.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import { Row, Col, Grid } from "react-bootstrap";
 import { Card } from "components/Card/Card.jsx";
+import "../assets/css/app.css";
 class Pos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+  componentDidMount() {
+    fetch("http://localhost:3001/GetAllProducts")
+      .then(resp => resp.json())
+      .then(prods => {
+        this.setState({ products: prods.data });
+      });
+  }
   render() {
     return (
       <div className="content">
@@ -9,11 +21,27 @@ class Pos extends Component {
           <Row>
             <Col md={12}>
               <Card
-                title="POS"
                 content={
-                  <div>
-                    <p>inicio</p>
-                  </div>
+                  <Row>
+                    <Col md={4} className="calculator">
+                      <p>inicio</p>
+                    </Col>
+                    <Col md={8}>
+                      <Row>
+                        <div style={{ overflowY: "auto !important" }}>
+                          {this.state.products.map(prod => {
+                            return (
+                              <Col md={3} style={{ height: "auto" }}>
+                                <Row>
+                                  <img src={"https://robohash.org/" + prod.id_producto} />
+                                </Row>
+                              </Col>
+                            );
+                          })}
+                        </div>
+                      </Row>
+                    </Col>
+                  </Row>
                 }
               />
             </Col>
